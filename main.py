912ad73bda44551a921dabdbd410a515b45803e6
iotarepeat <dhruvam15@gmail.com>
2019-01-16 19:13:06 +0530

    Created a class

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import tkinter as tk
 # board = [[0] * 4] * 4
-board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-root = tk.Tk()
-for i in range(len(board)):
-	for j in range(len(board)):
-		tk.Label(root,text=board[i][j],height=5,width=10,borderwidth=2,relief="ridge").grid(row=i,column=j)
-root.mainloop()
+class Main:
+	board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+	root = tk.Tk()
+	def display(self):
+		for i in range(len(self.board)):
+			for j in range(len(self.board)):
+				tk.Label(self.root,text=self.board[i][j],height=5,width=10,borderwidth=2,relief="ridge").grid(row=i,column=j)
+		self.root.mainloop()
+m = Main()
+m.display()
