10693864f5aa83167400698fd7a29baea133acd5
iotarepeat <dhruvam15@gmail.com>
2019-01-17 22:02:54 +0530

    Added moves

--- main.py
+++ main.py
@@ -4,16 +4,50 @@ import random, time
 
 # board = [[0] * 4] * 4
 class Main:
-	board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-	root = tk.Tk()
-	def key(self,event):
-		print("Pressed",event.keysym)
-	def wait_for_move(self):
-		self.root.bind("<Up>",self.key)
-		self.root.bind("<Down>",self.key)
-		self.root.bind("<Left>",self.key)
-		self.root.bind("<Right>",self.key)
+	def __init__(self):
+		self.board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+		self.root = tk.Tk()
+		self.root.bind("<KeyPress>", self.key)
+
+	@staticmethod
+	def perform_move(l):
+		for i in range(len(l)):
+			if l[i] == 0:
+				del l[i]
+				l.insert(0, 0)
+		if l.count(0) >= 3:
+			return l
+		n = l[1:]
+		for i in range(len(l) - 1):
+			if l[i] == n[i]:
+				l[i] *= 2
+				l[i + 1] = 0
+		for i in range(len(l)):
+			if l[i] == 0:
+				del l[i]
+				l.insert(0, 0)
+		return l
+
+	def key(self, event):
+		if event.keysym == "Right":
+			self.board = list(map(self.perform_move, self.board))
+
+		if event.keysym == "Left":
+			self.board = list(
+			    map(lambda x: self.perform_move(x[::-1])[::-1], self.board))
+		if event.keysym == "Up":
+			transpose = lambda l_of_l:[list(l) for l in zip(*l_of_l)]
+			self.board = list(map(lambda x: self.perform_move(x[::-1])[::-1],transpose(self.board)))
+			self.board = transpose(self.board)
+
+		if event.keysym == "Down":
+			transpose = lambda l_of_l:[list(l) for l in zip(*l_of_l)]
+			self.board = list(map(self.perform_move,transpose(self.board)))
+			self.board = transpose(self.board)
+		self.display()
+
 	def display(self):
+		self.spawn()
 		for i in range(len(self.board)):
 			for j in range(len(self.board)):
 				tk.Label(
@@ -45,7 +79,5 @@ class Main:
 
 
 m = Main()
-m.spawn()
 m.display()
-m.wait_for_move()
 m.root.mainloop()
